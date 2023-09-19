f45510d47ccbab4e0c36725470d1286d6ee119bf
edembicki <60304557+edembicki@users.noreply.github.com>
2023-09-19 19:52:43 -0300

    update map

--- src/views/tracker/Tracker.component.tsx
+++ src/views/tracker/Tracker.component.tsx
@@ -37,7 +37,7 @@ const Dashboard: React.FC = () => {
       >
         <MenuComponent />
       </Header>
-      <div className='tracker-wrapper' style={{height: 800, overflow: 'hidden'}}>
+      <div className='tracker-wrapper' style={{minHeight: 800, overflow: 'hidden'}}>
         <Tracker />
       </div>      
     </React.Fragment>
